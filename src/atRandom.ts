04f7b0dbf5c61550cce4d981c9c8ca558938f7d5
Ken Snyder <ken@ken.net>
2020-08-13 14:52:30 -0700

    slightly improved exclusion capability to atRandom

--- src/atRandom.ts
+++ src/atRandom.ts
@@ -1,5 +1,7 @@
-export function atRandom<T = any>(things: T[], excluding: T[] = []) {
-  things = things.filter((i) => !excluding.includes(i));
+export function atRandom<T = any>(things: T[], excluding: T[] | ((item: T) => T[]) = []) {
+  things = things.filter((i) =>
+    typeof excluding === "function" ? !excluding(i).includes(i) : !excluding.includes(i)
+  );
   const random = Math.floor(Math.random() * things.length);
   return things[random];
 }
