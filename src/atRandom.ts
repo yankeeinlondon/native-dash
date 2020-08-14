0e1239204a777a189372b270c7268b73612ad787
Ken Snyder <ken@ken.net>
2020-08-13 21:28:47 -0700

    test: added tests for atRandom fix: fixed function exceptions for atRandom

--- src/atRandom.ts
+++ src/atRandom.ts
@@ -1,6 +1,6 @@
 export function atRandom<T = any>(things: T[], excluding: T[] | ((item: T) => boolean) = []) {
   things = things.filter((i) =>
-    typeof excluding === "function" ? !excluding(i) : !excluding.includes(i)
+    typeof excluding === "function" ? excluding(i) : !excluding.includes(i)
   );
   const random = Math.floor(Math.random() * things.length);
   return things[random];
