8ea1472aa5dd2c6657dbc80d3f4c0ea06571b7b1
Ken Snyder <ken@ken.net>
2021-05-02 14:44:44 -0700

    fixed firstKey and lastKey

--- src/lastKey.ts
+++ src/lastKey.ts
@@ -2,8 +2,10 @@ import { IDictionary } from "./IDictionary";
 /**
  * **lastKey**
  *
- * returns the _last_ key in a dictionary
+ * returns the _last_ key in a dictionary or `false` if there are no
+ * keys in object passed in.
  */
-export function lastKey(dict: IDictionary): undefined | (string & keyof typeof dict) {
-  return Object.keys(dict).slice(-1).pop();
+export function lastKey<T extends {}>(dict: T): keyof T | false {
+  const key = Object.keys(dict).slice(-1).pop() as keyof T | undefined;
+  return key ? key : false;
 }
